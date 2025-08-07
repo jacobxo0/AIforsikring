c09ea014edeecc08b6c933b4dbac88f581414d3e
jacob <jacobxo0@gmail.com>
2025-08-07 20:36:52 +0200

    🔧 Move app to src/app structure for better Vercel compatibility

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true
-  },
-  trailingSlash: false
-}
+const nextConfig = {}
 
 module.exports = nextConfig
